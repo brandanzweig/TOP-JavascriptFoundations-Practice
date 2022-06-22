7790311d43ebcb644355e8fbff90d4b6f83f396c
Brandan Zweig <brandanzweig@gmail.com>
2022-06-22 15:23:57 -0400

    test git process

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,7 +1,7 @@
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
 for (let i = 1; i <= answer; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) { //Test
         console.log("FizzBuzz");
     } else if (i % 3 === 0) {
         console.log("Fizz");
